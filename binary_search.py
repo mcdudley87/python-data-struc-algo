ad0e630e690ba09ac63d131372c5b2ee60e581bd
Michael <sound.plus.fury@gmail.com>
2019-10-02 14:34:02 -0700

    added recursive binary search

--- binary_search.py
+++ binary_search.py
@@ -11,3 +11,19 @@ def binary_search(list, target):
       first = midpoint + 1
     else: 
       last = midpoint -1
+      
+  return None
+
+def verify(index):
+  if index is not None:
+    print("Target found at index: ", index)
+  else:
+    print("Target not found in list")
+    
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = binary_search(numbers, 2)
+verify(result)
+
+result = binary_search(numbers, 12)
+verify(result)
